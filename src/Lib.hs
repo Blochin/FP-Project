6e28449db08cc84c94943c58d3ef7e0caf23cf65
Lubomir Sevcik <lubomir.sevcik@salesxp.com>
2022-05-07 23:48:37 +0200

    mapped links on touples

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,18 +8,25 @@ module Lib
 import PageData ( PageData )
 import Parser ( parser, linkHtmlParser, something, linkParser )
 import Loader ( getHtml, loadData, getUrl )
+import Graph ( createGraph, markLinks)
 
 searchEngine :: IO [Maybe PageData] -> IO ()
 searchEngine pages = do
   unpackedPages <- pages :: IO[Maybe PageData]
-
-  let currentLinks = linkParser $ map(\s -> getUrl s) unpackedPages
-  let words = map(parser . getHtml) unpackedPages
-  let links = map(linkHtmlParser . getHtml) unpackedPages
   
-  print links
+  let words = map(parser . getHtml) unpackedPages
+  let currentLinks = linkParser $ map getUrl unpackedPages
+  let otherLinks = map(linkHtmlParser . getHtml) unpackedPages
+
+  let mappedLinks = map (\s -> (getUrl s, concatMap(linkHtmlParser . getHtml) unpackedPages )) unpackedPages
+  let mappedWords = map (\s -> (getUrl s, concatMap(parser . getHtml) unpackedPages )) unpackedPages
+
+  print $ markLinks currentLinks otherLinks
+  print $ createGraph mappedLinks
+  
 
 searchEngineModule :: IO ()
 searchEngineModule = do
     let decoded = loadData "data/data.json"
     searchEngine decoded
+
