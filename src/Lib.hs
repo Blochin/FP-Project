7d9b90ac2f6ea38250a24b3e3e0cf90140207682
Lubomir Sevcik <lubomir.sevcik@salesxp.com>
2022-05-09 18:33:48 +0200

    merge with grapsh

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,6 +10,7 @@ import Parser ( parser, linkHtmlParser, something, linkParser )
 import Loader ( getHtml, loadData, getUrl )
 import Graph ( createGraph, markLinks)
 import InvertedIndex (invertedIndex)
+import Data.Tuple.Select ( Sel2(sel2), Sel1 (sel1) )
 
 
 searchEngine :: IO [Maybe PageData] -> IO ()
@@ -21,14 +22,15 @@ searchEngine pages = do
   let otherLinks = map(linkHtmlParser . getHtml) unpackedPages
 
   let mappedLinks = map (\s -> (getUrl s, linkHtmlParser $ getHtml s )) unpackedPages
+  let mappedLinksButDifferent = map(\s->map(\s2->(sel1 s,s2)) $ sel2 s) mappedLinks
   let mappedWords = map (\s -> (getUrl s, parser $ getHtml s )) unpackedPages
   let mappedInvertedWords = invertedIndex "sete" mappedWords
 
-  --print mappedLinks
+  print mappedLinksButDifferent
   --print mappedWords
   --print $ markLinks currentLinks otherLinks
   --print $ createGraph mappedLinks
-  print mappedInvertedWords
+  --print mappedInvertedWords
   
 
 searchEngineModule :: IO ()
